107530fdd009bdc4608d5a52ec55bf30b9fb790c
Marco Ceccarelli <maceccar@c2r1p12.42firenze.it>
2024-06-26 13:52:03 +0200

    solved moves error

--- src/moves.c
+++ src/moves.c
@@ -19,39 +19,39 @@ void	rotation(t_stack *a);
 void	rx(t_stack *stack)
 {
 	ft_printf("r%c\n", stack->name);
-	reverse_rotation(stack);
+	rotation(stack);
 }
 
 void	rr(t_stack *a, t_stack *b)
 {
 	ft_printf("rr\n");
-	reverse_rotation(a);
-	reverse_rotation(b);
+	rotation(a);
+	rotation(b);
 }
 
 void	rrx(t_stack *stack)
 {
 	ft_printf("rr%c\n", stack->name);
-	rotation(stack);
+	reverse_rotation(stack);
 }
 
 void	rrr(t_stack *a, t_stack *b)
 {
 	ft_printf("rrr\n");
-	rotation(a);
-	rotation(b);
+	reverse_rotation(a);
+	reverse_rotation(b);
 }
 
 void	reverse_rotation(t_stack *a)
 {
 	if (ft_is_stack_empty(a) || a->length < 2)
 		return ;
-	a = ft_push_tail(a, ft_pop(a));
+	a = ft_push(a, ft_pop_tail(a));
 }
 
 void	rotation(t_stack *a)
 {
 	if (ft_is_stack_empty(a) || a->length < 2)
 		return ;
-	a = ft_push(a, ft_pop_tail(a));
+	a = ft_push_tail(a, ft_pop(a));
 }
